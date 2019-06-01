33dec441570199f91a41836d51461345434b90c9
pelaogon <pelaogon@gmail.com>
2019-06-01 18:57:04 -0400

    Variable de enterno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Pelaogon3105@ds153890.mlab.com:53890/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
