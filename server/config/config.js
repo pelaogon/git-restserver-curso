bb98db3c7a06d54a4e6110b9c420acdac0e408a7
pelaogon <pelaogon@gmail.com>
2019-06-01 18:43:12 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // ===========
 process.env.PORT = process.env.PORT || 3000;
+
+// ===========
+// Entorno
+// ===========
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ===========
+// Base de datos
+// ===========
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Pelaogon3105@ds153890.mlab.com:53890/cafe';
+}
+
+process.env.URLDB = urlDB;
