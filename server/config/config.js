de8dddee959fb3d0e3d47dfa7de4706c6bd46591
pelaogon <pelaogon@gmail.com>
2019-06-03 12:44:08 -0400

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -36,3 +36,10 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+
+// ===========
+// Google Client ID
+// ===========
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '582647251923-plhi301p1hnn91n6lro7r53l258lrfb0.apps.googleusercontent.com';
